9717fe43d3e319c2d3666be5eb1b7727fc5ca4ce
Adriano Ortiz Garda <adriano.ortizgarda@gmail.com>
2020-09-23 13:19:29 -0500

    commit con layout

---
+++ src/components/LayoutApp.js
@@ -0,0 +1,14 @@
+import React, {useState, useContext} from 'react'
+import {Link} from "react-router-dom"
+import {logOut} from "../services"
+import {MyContext} from "../context"
+
+function LayoutApp() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LayoutApp
